e16d0a37276974b2ed190fecee2c117fae380bc8
Pavel Matviyuk <pav.matt@mail.ru>
2025-04-07 23:47:18 +0300

    Add ficsit_test

--- ficsit_test.py
+++ ficsit_test.py
@@ -1,1 +1,20 @@
 # тест для проверки одинаковости input.bin и output.bin
+
+import ficsit
+
+def main():
+
+	io = ficsit.FicsitPipelineIO('input.bin')
+
+	sorter = ficsit.FicsitBlockSorter()
+	merger = ficsit.FicsitBlockMerger()
+
+	while io.HasBlock():
+		block = io.ReadBlock()
+		sorter.Sort(block)
+		io.WriteBlock(block)
+		if io.ReadyMerge():
+			merged_file = merger.Merge(io.LeftMergeFile(), io.RightMergeFile())
+			io.SetMergedFile(merged_fle)
+
+main()
