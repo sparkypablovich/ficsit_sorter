06907a7219a6259782208771cfe2d26ad8d7186d
sparky pablovich <maxmatviyuk@gmail.com>
2025-04-14 16:25:09 +0300

    merge update in ficsit

--- ficsit.py
+++ ficsit.py
@@ -1,8 +1,41 @@
+import struct
+import os
+
 # Соединитель отсортированных блоков в выходной конвеер
 
 class FicsitBlockMerger:
-	def Sort(self):
-		pass
+
+	result_filename = 'new_left.bin'
+
+	def Merge(self, leftfile, rightfile):
+		with open(leftfile) as lfile, open(rightfile) as rfile, open('result.txt', 'w') as res:
+			left = lfile.read(4)
+			right = rfile.read(4)
+			while True:
+				
+				if left == '':
+					self.writer(right)
+					right = rfile.read()
+					self.writer(right)
+					break
+				if right == '':
+					self.writer(left)
+					left = lfile.read()
+					self.writer(left)
+					break
+				
+				if int(left) <= int(right):
+					self.writer(left)
+					left = lfile.read(1)
+				else:
+					self.writer(right)
+					right = rfile.read(1)
+		return self.result_filename
+
+	def writer(self, number):
+		with open(self.result_filename, 'w+') as output:
+			output.write(number)
+
 	
     
 
@@ -31,10 +64,51 @@ class FicsitBlockSorter:
 	
 
 
+
 # Класс для работы с бинарными файлами
 
 class FicsitPipelineIO:
+	
+	def __init__(self, filename, block_size=4096): # block_size=32000
+		self.__filename = filename
+		self.__block_size = block_size
+
 	def ReadBlock(self):
-		pass
-	def WriteBlock(self):
-		pass
+		with open(self.__filename, 'rb') as file:
+			while True:
+				block = file.read(self.__block_size)
+				if not block:
+					break  # End of file
+				# Process the block here
+				yield block  # or do something else with the block
+
+
+
+	__leftmergefile = ''
+	__rightmergefile = ''
+
+	def WriteBlock(self, block):
+		if self.__leftmergefile == '':
+			filename = 'left.bin'
+			self.__leftmergefile = filename
+		else:
+			filename = 'right.bin'
+			self.__rightmergefile = filename
+
+		with open(filename, 'wb') as file:
+			file.write(block)
+
+
+	def ReadyMerge(self):
+		return self.LeftMergeFile() != '' and self.RightMergeFile() != ''
+		
+	def SetMergedFile(self, filename):
+		self.__leftmergefile = filename
+		os.rename(filename, 'left.bin')
+		self.__rightmergefile = ''
+
+	def LeftMergeFile(self):
+		return self.__leftmergefile
+
+	def RightMergeFile(self):
+		return self.__rightmergefile
