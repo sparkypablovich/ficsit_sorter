d4f516a0ee4bc1a3eaf4fb8d675e72e5856a3bd2
Pavel Matviyuk <pav.matt@mail.ru>
2025-04-15 20:13:57 +0300

    Fix general errors

--- ficsit.py
+++ ficsit.py
@@ -2,50 +2,47 @@ import struct
 import os
 
 # Соединитель отсортированных блоков в выходной конвеер
-
 class FicsitBlockMerger:
 
-	result_filename = 'new_left.bin'
+	__result_filename = 'new_left.bin'
 
 	def Merge(self, leftfile, rightfile):
-		with open(leftfile) as lfile, open(rightfile) as rfile, open('result.txt', 'w') as res:
+		with open(leftfile, 'rb') as lfile, open(rightfile, 'rb') as rfile:
 			left = lfile.read(4)
 			right = rfile.read(4)
 			while True:
-				
-				if left == '':
-					self.writer(right)
-					right = rfile.read()
-					self.writer(right)
-					break
-				if right == '':
-					self.writer(left)
-					left = lfile.read()
-					self.writer(left)
-					break
-				
-				if int(left) <= int(right):
-					self.writer(left)
-					left = lfile.read(1)
-				else:
-					self.writer(right)
-					right = rfile.read(1)
-		return self.result_filename
 
-	def writer(self, number):
-		with open(self.result_filename, 'w+') as output:
+				left_int = int.from_bytes(left, byteorder='little', signed=True)
+				right_int = int.from_bytes(right, byteorder='little', signed=True)
+				
+				if left == b'':
+					self.write(right)
+					right = rfile.read()
+					self.write(right)
+					break
+				if right == b'':
+					self.write(left)
+					left = lfile.read()
+					self.write(left)
+					break
+				
+				if left_int <= right_int:
+					self.write(left)
+					left = lfile.read(4)
+				else:
+					self.write(right)
+					right = rfile.read(4)
+
+		return self.__result_filename
+
+	def write(self, number):
+		with open(self.__result_filename, 'ab') as output:
 			output.write(number)
 
-	
-    
-
 # Сортировщик блоков
-
 class FicsitBlockSorter:
 
 	def Sort(self, alist):
-		if len(alist) <= 1:
-			return alist
 		self.qsort(alist, 0, len(alist) - 1)
 
 
@@ -60,32 +57,26 @@ class FicsitBlockSorter:
 						mid += 1
 			self.qsort(alist, start, mid - 1)
 			self.qsort(alist, mid + 1, end)
-		return alist
-	
-
 
 
 # Класс для работы с бинарными файлами
-
 class FicsitPipelineIO:
 	
-	def __init__(self, filename, block_size=4096): # block_size=32000
+	__leftmergefile = ''
+	__rightmergefile = ''
+
+	def __init__(self, filename, block_size=4096):
 		self.__filename = filename
 		self.__block_size = block_size
 
 	def ReadBlock(self):
 		with open(self.__filename, 'rb') as file:
 			while True:
-				block = file.read(self.__block_size)
+				block = file.read(self.__block_size * 4)
 				if not block:
-					break  # End of file
-				# Process the block here
-				yield block  # or do something else with the block
-
-
-
-	__leftmergefile = ''
-	__rightmergefile = ''
+					break
+				unpacked = [int.from_bytes(block[i:i+4], byteorder='little', signed=True) for i in range(0, len(block), 4)]
+				yield unpacked
 
 	def WriteBlock(self, block):
 		if self.__leftmergefile == '':
@@ -95,16 +86,23 @@ class FicsitPipelineIO:
 			filename = 'right.bin'
 			self.__rightmergefile = filename
 
+		packed = []
+		for number in block:
+			packed.append(int.to_bytes(number, length=4, byteorder='little', signed=True))
+					
+		packed_bytes = b''.join(packed)
+
 		with open(filename, 'wb') as file:
-			file.write(block)
+			file.write(packed_bytes)
 
 
 	def ReadyMerge(self):
 		return self.LeftMergeFile() != '' and self.RightMergeFile() != ''
 		
 	def SetMergedFile(self, filename):
-		self.__leftmergefile = filename
+		os.remove('left.bin')
 		os.rename(filename, 'left.bin')
+		self.__leftmergefile = 'left.bin'
 		self.__rightmergefile = ''
 
 	def LeftMergeFile(self):
--- ficsit_test.py
+++ ficsit_test.py
@@ -1,20 +1,21 @@
-# тест для проверки одинаковости input.bin и output.bin
-
 import ficsit
 
 def main():
 
-	io = ficsit.FicsitPipelineIO('input.bin')
+	io = ficsit.FicsitPipelineIO('input.bin', 16)
 
 	sorter = ficsit.FicsitBlockSorter()
 	merger = ficsit.FicsitBlockMerger()
 
-	while io.HasBlock():
-		block = io.ReadBlock()
+	for block_number, block in enumerate(io.ReadBlock()):
+		print(f'>> Processing Block #{block_number}')
 		sorter.Sort(block)
 		io.WriteBlock(block)
 		if io.ReadyMerge():
 			merged_file = merger.Merge(io.LeftMergeFile(), io.RightMergeFile())
-			io.SetMergedFile(merged_fle)
+			io.SetMergedFile(merged_file)
+	
+	# TODO
+	# io.SaveResultFile() # Сохранит результат в output.bin
 
 main()
